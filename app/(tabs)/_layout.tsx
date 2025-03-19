6fe0f7b3a1f191bbc3b8b5bba1d0341cf20cf48e
arifbiswas <mxgangstar310@gmail.com>
2025-03-19 16:04:43 +0600

    git add fixing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,86 +1,97 @@
-import { IconSymbol } from "@/app-example/components/ui/IconSymbol";
-import { Stack, Tabs, useRouter } from "expo-router";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
+import { Tabs, useRouter } from "expo-router";
+
 import { StatusBar } from "expo-status-bar";
-import { FontAwesome } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native"; // To make the back button clickable
 
 export default function TabsLayout() {
-    const router = useRouter();
-    return (
-        <>
-            <StatusBar
-                style="light"
-                backgroundColor="#4CAF50"
-                translucent={false}
-                hidden={false}
-            />
-            <Tabs
-                screenOptions={{
-                    tabBarActiveTintColor: "#FFFBE6",
-                    tabBarInactiveTintColor: "#C0EBA6",
-                    tabBarActiveBackgroundColor: "#388E3C",
-                    tabBarInactiveBackgroundColor: "#388E3C",
-                    headerStyle: {
-                        backgroundColor: "#4CAF50",
-                    },
-                    headerTitleStyle: {
-                        color: "#FDFAF6",
-                        fontSize: 25,
-                    },
-                    tabBarLabelStyle: {
-                        fontWeight: "bold",
-                        textAlign: "center",
-                    },
-                }}
-            >
-                <Tabs.Screen
-                    name="index"
-                    options={{
-                        headerTitle: "যাকাত",
-                        title: "Home",
-                        tabBarIcon: ({ color }) => (
-                            <IconSymbol size={28} name="house.fill" color={color} />
-                        ),
-                        headerLeft: () => (
-                            <TouchableOpacity style={{ marginLeft: 15 }} onPress={() => router.back()}>
-                                <FontAwesome name="asl-interpreting" size={24} color="#FDFAF6" />
-                            </TouchableOpacity>
-                        ),
-                    }}
+  const router = useRouter();
+  return (
+    <>
+      <StatusBar
+        style="light"
+        backgroundColor="#4CAF50"
+        translucent={false}
+        hidden={false}
+      />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFFBE6",
+          tabBarInactiveTintColor: "#C0EBA6",
+          tabBarActiveBackgroundColor: "#388E3C",
+          tabBarInactiveBackgroundColor: "#388E3C",
+          headerStyle: {
+            backgroundColor: "#4CAF50",
+          },
+          headerTitleStyle: {
+            color: "#FDFAF6",
+            fontSize: 25,
+          },
+          tabBarLabelStyle: {
+            fontWeight: "bold",
+            textAlign: "center",
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            headerTitle: "যাকাত",
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <Entypo size={20} name="home" color={color} />
+            ),
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{ marginLeft: 15 }}
+                onPress={() => router.back()}
+              >
+                <FontAwesome
+                  name="asl-interpreting"
+                  size={24}
+                  color="#FDFAF6"
                 />
-                <Tabs.Screen
-                    name="ZakatGuidePage"
-                    options={{ 
-                        headerTitle: "Zakat Guide",
-                        tabBarLabel: "Zakat Guide",
-                        tabBarIcon: ({ color }) => (
-                            <FontAwesome name="book" size={20} color={color} />
-                        ),
-                        headerLeft: () => (
-                            <TouchableOpacity style={{ marginLeft: 15, marginRight: 15 }} onPress={() => router.back()}>
-                                <FontAwesome name="arrow-left" size={24} color="#FDFAF6" />
-                            </TouchableOpacity>
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name="Zakat"
-                    options={{
-                        headerTitle: "যাকাত ক্যালকুলেটর",
-                        tabBarLabel: "Calculator",
-                        tabBarIcon: ({ color }) => (
-                            <FontAwesome name="calculator" size={20} color={color} />
-                        ),
-                        headerLeft: () => (
-                            <TouchableOpacity style={{ marginLeft: 15, marginRight: 15 }} onPress={() => router.back()}>
-                                <FontAwesome name="arrow-left" size={24} color="#FDFAF6" />
-                            </TouchableOpacity>
-                        ),
-                    }}
-                />
-
-
-            </Tabs>
-        </>
-    );
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ZakatGuidePage"
+          options={{
+            headerTitle: "Zakat Guide",
+            tabBarLabel: "Zakat Guide",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="book" size={20} color={color} />
+            ),
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{ marginLeft: 15, marginRight: 15 }}
+                onPress={() => router.back()}
+              >
+                <FontAwesome name="arrow-left" size={24} color="#FDFAF6" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Zakat"
+          options={{
+            headerTitle: "যাকাত ক্যালকুলেটর",
+            tabBarLabel: "Calculator",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="calculator" size={20} color={color} />
+            ),
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{ marginLeft: 15, marginRight: 15 }}
+                onPress={() => router.back()}
+              >
+                <FontAwesome name="arrow-left" size={24} color="#FDFAF6" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
 }
